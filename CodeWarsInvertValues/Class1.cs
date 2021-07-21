46726025a53a245219a30d4de47cd5af6149213f
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-07-21 05:02:37 -0600

    Add project files.

---
+++ CodeWarsInvertValues/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using NUnit.Compatibility;
+
+namespace CodeWarsInvertValues
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new int[] { -1, -2, -3, -4, -5 }, ArraysInversion.InvertValues(new int[] { 1, 2, 3, 4, 5 }));
+            Assert.AreEqual(new int[] { -1, 2, -3, 4, -5 }, ArraysInversion.InvertValues(new int[] { 1, -2, 3, -4, 5 }));
+            Assert.AreEqual(new int[] { }, ArraysInversion.InvertValues(new int[] { }));
+            Assert.AreEqual(new int[] { 0 }, ArraysInversion.InvertValues(new int[] { 0 }));
+        }
+    }
+
+        public static class ArraysInversion
+        {
+            public static int[] InvertValues(int[] input)
+            {
+           // return input.Select(n => -n).ToArray();
+            return input.Select(d => -d).ToArray();
+            }
+        }
+    
+}
